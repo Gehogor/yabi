a367b9a05208f53bc0fd03c8e2baaced56917068
Jérôme Chemouny <jerome.chemouny.04@eigsi.fr>
2016-06-04 12:58:09 +0200

    Add globals functions for sharing variable interuption safe.

---
+++ SafeVariable.c
@@ -0,0 +1,156 @@
+/*
+ *__     __        ____  _____
+ *\ \   / / /\    |  _ \|_   _|
+ * \ \_/ / /  \   | |_) | | |
+ *  \   / / /\ \  |  _ <  | |
+ *   | | / ____ \ | |_) |_| |_
+ *   |_|/_/    \_\|____/|_____|
+ *
+ * File:   SharedVarLib.c
+ * Author: Yann
+ *
+ * Created on 25 avril 2016, 19:01
+ */
+
+#include "SafeVariable.h"
+
+
+// Management of unsigned char -----------------------------------------------//
+void init_u8(safe_u8* var)
+{
+    var->index = 0;
+    var->data[0] = 0;
+    var->data[1] = 0;
+}
+
+void write_u8(safe_u8* var, unsigned char value)
+{
+    var->index = inverseIndex(var->index);
+    var->data[ var->index ] = value;
+}
+
+unsigned char read_u8(safe_u8* var)
+{
+    if( var->index == 0 )
+        return var->data[1];
+    return var->data[0];
+}
+
+
+// Management of unsigned int ------------------------------------------------//
+void init_u16(safe_u16* var)
+{
+    var->index = 0;
+    var->data[0] = 0;
+    var->data[1] = 0;
+}
+
+void write_u16(safe_u16* var, unsigned int value)
+{
+    var->index = inverseIndex(var->index);
+    var->data[ var->index ] = value;
+}
+
+unsigned int read_u16(safe_u16* var)
+{
+    if( var->index == 0 )
+        return var->data[1];
+    return var->data[0];
+}
+
+
+// Management of unsigned long int -------------------------------------------//
+void init_u32(safe_u32* var)
+{
+    var->index = 0;
+    var->data[0] = 0;
+    var->data[1] = 0;
+}
+
+void write_u32(safe_u32* var, unsigned long int value)
+{
+    var->index = inverseIndex(var->index);
+    var->data[ var->index ] = value;
+}
+
+unsigned long int read_u32(safe_u32* var)
+{
+    if( var->index == 0 )
+        return var->data[1];
+    return var->data[0];
+}
+
+
+// Management of signed char -------------------------------------------------//
+void init_s8(safe_s8* var)
+{
+    var->index = 0;
+    var->data[0] = 0;
+    var->data[1] = 0;
+}
+
+void write_s8(safe_s8* var, signed char value)
+{
+    var->index = inverseIndex(var->index);
+    var->data[ var->index ] = value;
+}
+
+signed char read_s8(safe_s8* var)
+{
+    if( var->index == 0 )
+        return var->data[1];
+    return var->data[0];
+}
+
+
+// Management of signed int --------------------------------------------------//
+void init_s16(safe_s16* var)
+{
+    var->index = 0;
+    var->data[0] = 0;
+    var->data[1] = 0;
+}
+
+void write_s16(safe_s16* var, signed int value)
+{
+    var->index = inverseIndex(var->index);
+    var->data[ var->index ] = value;
+}
+
+signed int read_s16(safe_s16* var)
+{
+    if( var->index == 0 )
+        return var->data[1];
+    return var->data[0];
+}
+
+
+// Management of signed long -------------------------------------------------//
+void init_s32(safe_s32* var)
+{
+    var->index = 0;
+    var->data[0] = 0;
+    var->data[1] = 0;
+}
+
+void write_s32(safe_s32* var, signed long int value)
+{
+    var->index = inverseIndex(var->index);
+    var->data[ var->index ] = value;
+}
+
+signed long int read_s32(safe_s32* var)
+{
+    if( var->index == 0 )
+        return var->data[1];
+    return var->data[0];
+}
+
+
+// Tools ---------------------------------------------------------------------//
+unsigned char inverseIndex(unsigned char index)
+{
+    if( index == 0 )
+        return 1;
+    return 0;
+}
---
+++ SafeVariable.h
@@ -0,0 +1,74 @@
+/*
+ *__     __        ____  _____
+ *\ \   / / /\    |  _ \|_   _|
+ * \ \_/ / /  \   | |_) | | |
+ *  \   / / /\ \  |  _ <  | |
+ *   | | / ____ \ | |_) |_| |_
+ *   |_|/_/    \_\|____/|_____|
+ *
+ * File:   SharedVarLib.h
+ * Author: Yann
+ *
+ * Created on 25 avril 2016, 19:01
+ */
+
+#ifndef _SHAREDVARLIB__H
+#define	_SHAREDVARLIB_H_
+
+typedef struct {
+    unsigned char index;
+    unsigned char data[2];
+} safe_u8;
+
+typedef struct {
+    unsigned char index;
+    unsigned int data[2];
+} safe_u16;
+
+typedef struct {
+    unsigned char index;
+    unsigned long int data[2];
+} safe_u32;
+
+typedef struct {
+    unsigned char index;
+    signed char data[2];
+} safe_s8;
+
+typedef struct {
+    unsigned char index;
+    signed int data[2];
+} safe_s16;
+
+typedef struct {
+    unsigned char index;
+    signed long int data[2];
+} safe_s32;
+
+void init_u8(safe_u8* var);
+void write_u8(safe_u8* var, unsigned char value);
+unsigned char read_u8(safe_u8* var);
+
+void init_u16(safe_u16* var);
+void write_u16(safe_u16* var, unsigned int value);
+unsigned int read_u16(safe_u16* var);
+
+void init_u32(safe_u32* var);
+void write_u32(safe_u32* var, unsigned long int value);
+unsigned long int read_u32(safe_u32* var);
+
+void init_s8(safe_s8* var);
+void write_s8(safe_s8* var, signed char value);
+signed char read_s8(safe_s8* var);
+
+void init_s16(safe_s16* var);
+void write_s16(safe_s16* var, signed int value);
+signed int read_s16(safe_s16* var);
+
+void init_s32(safe_s32* var);
+void write_s32(safe_s32* var, signed long int value);
+signed long int read_u32(safe_s32* var);
+
+unsigned char inverseIndex(unsigned char index);
+
+#endif	/* _SHAREDVARLIB_H */
