cdc25354ef2c4aadb888cad5d725573695e9039a
Jérôme Chemouny <jerome.chemouny.04@eigsi.fr>
2016-06-05 16:34:55 +0200

    Add the possibility to initialize the structure.

--- SafeVariable.c
+++ SafeVariable.c
@@ -16,11 +16,11 @@
 
 
 // Management of unsigned char -----------------------------------------------//
-void init_u8(safe_u8* var)
+void init_u8(safe_u8* var, unsigned char value)
 {
     var->index = 0;
-    var->data[0] = 0;
-    var->data[1] = 0;
+    var->data[0] = value;
+    var->data[1] = value;
 }
 
 void write_u8(safe_u8* var, unsigned char value)
@@ -38,11 +38,11 @@ unsigned char read_u8(safe_u8* var)
 
 
 // Management of unsigned int ------------------------------------------------//
-void init_u16(safe_u16* var)
+void init_u16(safe_u16* var, unsigned int value)
 {
     var->index = 0;
-    var->data[0] = 0;
-    var->data[1] = 0;
+    var->data[0] = value;
+    var->data[1] = value;
 }
 
 void write_u16(safe_u16* var, unsigned int value)
@@ -60,11 +60,11 @@ unsigned int read_u16(safe_u16* var)
 
 
 // Management of unsigned long int -------------------------------------------//
-void init_u32(safe_u32* var)
+void init_u32(safe_u32* var, unsigned long int value)
 {
     var->index = 0;
-    var->data[0] = 0;
-    var->data[1] = 0;
+    var->data[0] = value;
+    var->data[1] = value;
 }
 
 void write_u32(safe_u32* var, unsigned long int value)
@@ -82,11 +82,11 @@ unsigned long int read_u32(safe_u32* var)
 
 
 // Management of signed char -------------------------------------------------//
-void init_s8(safe_s8* var)
+void init_s8(safe_s8* var, signed char value)
 {
     var->index = 0;
-    var->data[0] = 0;
-    var->data[1] = 0;
+    var->data[0] = value;
+    var->data[1] = value;
 }
 
 void write_s8(safe_s8* var, signed char value)
@@ -104,11 +104,11 @@ signed char read_s8(safe_s8* var)
 
 
 // Management of signed int --------------------------------------------------//
-void init_s16(safe_s16* var)
+void init_s16(safe_s16* var, signed int value)
 {
     var->index = 0;
-    var->data[0] = 0;
-    var->data[1] = 0;
+    var->data[0] = value;
+    var->data[1] = value;
 }
 
 void write_s16(safe_s16* var, signed int value)
@@ -126,11 +126,11 @@ signed int read_s16(safe_s16* var)
 
 
 // Management of signed long -------------------------------------------------//
-void init_s32(safe_s32* var)
+void init_s32(safe_s32* var, signed long int value)
 {
     var->index = 0;
-    var->data[0] = 0;
-    var->data[1] = 0;
+    var->data[0] = value;
+    var->data[1] = value;
 }
 
 void write_s32(safe_s32* var, signed long int value)
--- SafeVariable.h
+++ SafeVariable.h
@@ -45,27 +45,27 @@ typedef struct {
     signed long int data[2];
 } safe_s32;
 
-void init_u8(safe_u8* var);
+void init_u8(safe_u8* var, unsigned char value);
 void write_u8(safe_u8* var, unsigned char value);
 unsigned char read_u8(safe_u8* var);
 
-void init_u16(safe_u16* var);
+void init_u16(safe_u16* var, unsigned int value);
 void write_u16(safe_u16* var, unsigned int value);
 unsigned int read_u16(safe_u16* var);
 
-void init_u32(safe_u32* var);
+void init_u32(safe_u32* var, unsigned long int value);
 void write_u32(safe_u32* var, unsigned long int value);
 unsigned long int read_u32(safe_u32* var);
 
-void init_s8(safe_s8* var);
+void init_s8(safe_s8* var, signed char value);
 void write_s8(safe_s8* var, signed char value);
 signed char read_s8(safe_s8* var);
 
-void init_s16(safe_s16* var);
+void init_s16(safe_s16* var, signed int value);
 void write_s16(safe_s16* var, signed int value);
 signed int read_s16(safe_s16* var);
 
-void init_s32(safe_s32* var);
+void init_s32(safe_s32* var, signed long int value);
 void write_s32(safe_s32* var, signed long int value);
 signed long int read_u32(safe_s32* var);
 
